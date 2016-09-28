a8a37dd5395cb0935416406b46cc46ca8c970999
James Ferguson <fergfamster@gmail.com>
2016-09-28 12:46:10 -0700

    Changes to main site template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1495,7 +1495,7 @@ $(".rotate").textrotator({
 
 	/* discus */
 	/* * * CONFIGURATION VARIABLES * * */
-	var disqus_shortname = 'reigntemplate';
+	var disqus_shortname = 'thnkUtility';
 
 	/* * * DON'T EDIT BELOW THIS LINE * * */
 	(function() {
