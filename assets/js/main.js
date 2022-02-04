27e57607a18c7e101f2df1e8f9b156dba311a1bf
Benjamin Michaelis <benjamin@michaelis.net>
2022-02-04 12:14:07 -0800

    Update map location

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,7 +87,7 @@ $(document).ready(function () {
             zoom: 11,
             scrollwheel: false,
             // The latitude and longitude to center the map (always required)
-            center: new google.maps.LatLng(47.6702238,-117.282961), // WA
+            center: new google.maps.LatLng(47.65289551565907, -117.43814726934703), // WA
 
             // How you would like to style the map.
             // This is where you would paste any style found on Snazzy Maps.
@@ -103,7 +103,7 @@ $(document).ready(function () {
 
         // Let's also add a marker while we're at it
         var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(47.6702238,-117.282961),
+            position: new google.maps.LatLng(47.65289551565907, -117.43814726934703),
             map: map,
             title: 'Snazzy!'
         });
